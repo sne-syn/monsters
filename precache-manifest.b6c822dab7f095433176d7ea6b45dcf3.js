2c78bb0019574152979446d03c4c84ea7f4bd13f
sne-syn <snezhana.syngur@gmail.com>
2020-09-04 19:29:05 +0200

    Updates

---
+++ precache-manifest.b6c822dab7f095433176d7ea6b45dcf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c745985354fe8f73d1126ee4bb085f3d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "4dd514e2cb93c8da8578",
+    "url": "/monsters/static/css/main.2ef6dbaa.chunk.css"
+  },
+  {
+    "revision": "cb878776b864e4de8aae",
+    "url": "/monsters/static/js/2.62bd6c74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.62bd6c74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dd514e2cb93c8da8578",
+    "url": "/monsters/static/js/main.dd6a438f.chunk.js"
+  },
+  {
+    "revision": "8c3889d3c0bb81ba4ee3",
+    "url": "/monsters/static/js/runtime-main.1510dc47.js"
+  }
+]);
