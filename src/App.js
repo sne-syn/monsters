593733bc0749f5f400704d0880cb20ab81920a34
sne-syn <snezhana.syngur@gmail.com>
2020-09-04 11:39:54 +0200

    new: App styling

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
 
     return (
       <div className="App">
+      <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={evt => this.setState({ searchField: evt.target.value })} />
